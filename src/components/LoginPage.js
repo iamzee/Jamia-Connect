cb61b444a34d09e48ceeb9c3bac318dfaf658d74
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2018-07-15 23:11:26 +0530

    Styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux';
 import { startLogin } from './../actions/auth';
 
 const LoginPage = props => (
-    <div>
-        <button onClick={startLogin}>Login with Google</button>
+    <div className="login-page">
+        <div className="login-page__box">
+            <h1 className="login-page__title">Jamians Rivet</h1>
+            <p className="login-page__subtitle">
+                Connecting minds along with the essence of heart
+            </p>
+            <button className="button button--login" onClick={startLogin}>
+                Login with Google
+            </button>
+        </div>
     </div>
 );
 
