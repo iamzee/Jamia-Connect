97e7031a93b727ad713494a015e539c1d28def4d
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2018-07-16 02:05:03 +0530

    Styling notes dashboard

--- src/components/NotesDashboard.js
+++ src/components/NotesDashboard.js
@@ -3,15 +3,54 @@ import { Link } from 'react-router-dom';
 import UploadNotesForm from './UploadNotesForm';
 
 const NotesDashboard = () => (
-    <div>
-        <h2>Notes Dashboard</h2>
-        <UploadNotesForm />
-        <Link to="/notes/first_year">First Year</Link>
-        <Link to="/notes/computer_science">Computer Science</Link>
-        <Link to="/notes/electrical">Electrical</Link>
-        <Link to="/notes/electronics">Electronics</Link>
-        <Link to="/notes/mechanical">Mechanical</Link>
-        <Link to="/notes/civil">Civil</Link>
+    <div className="notes-dashboard">
+        <div className="notes-dashboard__content">
+            <div className="notes-dashboard__header">
+                <h2 className="notes-dashboard__title">Notes Dashboard</h2>
+                <button className="button notes-dashboard__button">
+                    Upload Notes
+                </button>
+            </div>
+            {/*<UploadNotesForm />*/}
+            <div className="notes-dashboard__branches">
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/first_year"
+                >
+                    First Year
+                </Link>
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/computer_science"
+                >
+                    Computer Science
+                </Link>
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/electrical"
+                >
+                    Electrical
+                </Link>
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/electronics"
+                >
+                    Electronics
+                </Link>
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/mechanical"
+                >
+                    Mechanical
+                </Link>
+                <Link
+                    className="notes-dashboard__branches-link"
+                    to="/notes/civil"
+                >
+                    Civil
+                </Link>
+            </div>
+        </div>
     </div>
 );
 
